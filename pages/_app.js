c69b2b6cdb0e682f47ceba96c6fa4f6f8db74266
Riyaz Alvi <89968985+alvisahab8810@users.noreply.github.com>
2026-02-20 14:31:54 +0530

    changes done

--- pages/_app.js
+++ pages/_app.js
@@ -234,7 +234,7 @@ function MyApp({ Component, pageProps }) {
     {/* ========================= */}
     {/* ✅ Meta Pixel */}
     {/* ========================= */}
-    <Script id="meta-pixel" strategy="afterInteractive">
+    {/* <Script id="meta-pixel" strategy="afterInteractive">
       {`
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -249,7 +249,38 @@ function MyApp({ Component, pageProps }) {
         fbq('init', '1032968578803017');
         fbq('track', 'PageView');
       `}
-    </Script>
+    </Script> */}
+
+
+     {/* Meta Pixel Code */}
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '25558322930503620');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=25558322930503620&ev=PageView&noscript=1"
+        />
+      </noscript>
 
     {/* ========================= */}
     {/* ✅ GTM Noscript */}
