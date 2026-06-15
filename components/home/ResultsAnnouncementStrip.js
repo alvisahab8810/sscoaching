fe2506ef7d20cdd7d27b62df04cbf13f6729592b
alvisahab8810 <alvisahab1999@gmail.com>
2026-06-15 11:41:24 +0530

    changes done

--- components/home/ResultsAnnouncementStrip.js
+++ components/home/ResultsAnnouncementStrip.js
@@ -23,7 +23,7 @@ export default function ResultsAnnouncementStrip() {
           {/* Desktop text */}
           <p className="ras-text">
             <span className="ras-star" aria-hidden="true">★</span>
-            &nbsp;NIOS&nbsp;<strong>Senior Secondary (Class 12th) Results are Out Now!</strong>
+            &nbsp;NIOS&nbsp;<strong>Secondary (Class 10th) Results are Out Now!</strong>
             &nbsp;— Check your result instantly with your Enrolment Number.&nbsp;
             <span className="ras-star" aria-hidden="true">★</span>
           </p>
@@ -31,7 +31,7 @@ export default function ResultsAnnouncementStrip() {
           {/* Mobile marquee */}
           <div className="ras-marquee-wrap" aria-hidden="true">
             <span className="ras-marquee">
-              ★&nbsp; NIOS Senior Secondary (Class&nbsp;12th) Results are Out Now! — Check your result with your Enrolment Number. &nbsp;&nbsp;&nbsp;★&nbsp; NIOS Senior Secondary (Class&nbsp;12th) Results are Out Now! — Check your result with your Enrolment Number. &nbsp;&nbsp;&nbsp;
+              ★&nbsp; NIOS Secondary (Class&nbsp;10th) Results are Out Now! — Check your result with your Enrolment Number. &nbsp;&nbsp;&nbsp;★&nbsp; NIOS Secondary (Class&nbsp;10th) Results are Out Now! — Check your result with your Enrolment Number. &nbsp;&nbsp;&nbsp;
             </span>
           </div>
 
